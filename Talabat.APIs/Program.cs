295d2c7dc0f417d825dc9ae4b151525ab4eb4283
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-04-10 19:59:05 +0200

    Update Database

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -6,8 +6,9 @@ namespace Talabat.APIs
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+
             var webApplicationBuilder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
@@ -24,6 +25,26 @@ namespace Talabat.APIs
 
 
             var app = webApplicationBuilder.Build();
+
+
+            using var scope = app.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            var _dbContext = services.GetRequiredService<StoreContext>();
+
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                await _dbContext.Database.MigrateAsync(); // Update Database
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+
+                logger.LogError(ex, "An Error has been occured during apply the migration");
+            }
 
             // Configure the HTTP request pipeline.
             #region Configure Kestrel Middlewares
