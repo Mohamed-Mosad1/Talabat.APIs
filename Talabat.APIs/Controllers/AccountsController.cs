92cff06da085a7e42fd77dd0f7c9dca9c7436213
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-04-26 15:07:28 +0300

    Register Endpoint

---
+++ Talabat.APIs/Controllers/AccountsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.Dtos;
+using Talabat.APIs.Error;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.APIs.Controllers
+{
+    public class AccountsController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        // Register
+        [HttpPost("Register")] // POST : /api/Accounts/Register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+        {
+            var user = new AppUser()
+            {
+                DisplayName = model.DisplayName,
+                Email = model.Email,
+                UserName = model.Email.Split("@")[0],
+                PhoneNumber = model.PhoneNumber
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded) return BadRequest(new ApiResponse(400));
+
+            var returnedUser = new UserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = "ThisWillBeToken"
+            };
+
+            return Ok(returnedUser);
+        }
+
+        // Login
+
+
+
+
+    }
+}
---
+++ Talabat.APIs/Dtos/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talabat.APIs.Dtos
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string DisplayName { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{8,}$)((?=.*\\d)|(?=.*[\\W_]))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$", ErrorMessage = "Password must be eight characters including one uppercase letter, one special character and alphanumeric characters")]
+        public string Password { get; set; }
+    }
+}
