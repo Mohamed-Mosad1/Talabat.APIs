7139c24c897910b38253162f714921028a545cce
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-05-05 23:02:25 +0300

    Add Admin Dashboard To Project

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -34,7 +34,10 @@ namespace Talabat.APIs
 
             webApplicationBuilder.Services.addApplicationServices();
 
-            webApplicationBuilder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection")));
+            webApplicationBuilder.Services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             webApplicationBuilder.Services.AddDbContext<AppIdentityDbContext>(options =>
             {
