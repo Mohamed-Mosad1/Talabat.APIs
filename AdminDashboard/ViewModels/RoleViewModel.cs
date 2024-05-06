c1366cbd171802fdea745306bf4db211106e1cef
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-05-06 23:59:55 +0300

    Manage Users & Roles

---
+++ AdminDashboard/ViewModels/RoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AdminDashboard.ViewModels
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ AdminDashboard/ViewModels/UserRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AdminDashboard.ViewModels
+{
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public List<RoleViewModel> Roles { get; set; }
+    }
+}
