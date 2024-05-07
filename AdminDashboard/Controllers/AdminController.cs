052c58c28c38eed0162d0221283c1c3581306981
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-05-07 18:23:44 +0300

    Add AdminController With Login & Logout

---
+++ AdminDashboard/Controllers/AdminController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.Dtos;
+using Talabat.Core.Entities.Identity;
+
+namespace AdminDashboard.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AdminController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user is null)
+            {
+                ModelState.AddModelError("Email", "Invalid Email");
+                return RedirectToAction(nameof(Login));
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded || !await _userManager.IsInRoleAsync(user, "Admin"))
+            {
+                ModelState.AddModelError(string.Empty, "You are not Authorithezed");
+                return RedirectToAction(nameof(Login));
+            }
+
+            return RedirectToAction(nameof(Index), "Home");
+        }
+
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+
+
+
+
+    }
+}
