68709d483d2ba48b4532e61e8785e8cd98271937
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-05-08 18:10:54 +0300

    GET Products & Create & Edit & Delete Product & Get Product Brands

---
+++ AdminDashboard/Helpers/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AdminDashboard.ViewModels;
+using AutoMapper;
+using Talabat.Core.Entities.Product;
+
+namespace AdminDashboard.Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductViewModel>()
+    .ForMember(dest => dest.BrandId, opt => opt.MapFrom(src => src.BrandId))
+    .ForMember(dest => dest.Brand, opt => opt.MapFrom(src => src.Brand.Name))
+    .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId))
+    .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category.Name))
+    .ReverseMap();
+
+
+        }
+    }
+}
--- Talabat.Repository/Data/Config/ProductConfig/ProductBrandConfigurations.cs
+++ Talabat.Repository/Data/Config/ProductConfig/ProductBrandConfigurations.cs
@@ -1,19 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Talabat.Core.Entities.Product;
 
 namespace Talabat.Repository.Data.Config.ProductConfig
 {
-    internal class ProductBrandConfigurations : IEntityTypeConfiguration<ProductBrand>
+	internal class ProductBrandConfigurations : IEntityTypeConfiguration<ProductBrand>
     {
         public void Configure(EntityTypeBuilder<ProductBrand> builder)
         {
             builder.Property(B => B.Name).IsRequired();
+            builder.HasIndex(B => B.Name).IsUnique();
         }
     }
 }
